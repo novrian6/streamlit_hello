927ec0b3003384a7bbe45f366e0572aa73204b8d
novrian <novrian@karyavirtual.com>
2024-05-24 07:28:03 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,6 @@
+import streamlit as st
+st.title ("Hi I am streamlit")
+st.subheader("Hi I am your subheader")
+st.header ("I am header")
+st.text("Hi I am text")
+st.markdown("Hello this is markdown")
